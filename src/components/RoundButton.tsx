9da40ec549ef00f4739a3a8b18a518e13e323eca
rakshian24 <rakshithrao123@gmail.com>
2022-11-26 13:54:22 +0530

    feat: init commit

---
+++ src/components/RoundButton.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RoundButton = () => {
+  return (
+    <View>
+      <Text>RoundButton</Text>
+    </View>
+  )
+}
+
+export default RoundButton
+
+const styles = StyleSheet.create({})
